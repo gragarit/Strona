009eb1851cb4e165da53cc6881195002927add19
gragarit <damianxkox@gmail.com>
2021-10-21 17:32:58 +0200

    dodanie jsa

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const translate = document.querySelectorAll(".translate");
+const big_title = document.querySelector(".big-title");
+const header = document.querySelector("header");
+const shadow = document.querySelector(".shadow");
+const content = document.querySelector(".content");
+const section = document.querySelector("section");
+
+let header_height = header.offsetHeight;
+let section_height = section.offsetHeight;
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+
+window.addEventListener('scroll',  ( ) => {
+    let scroll = window.pageYOffset;
+    let sectionY = section.getBoundingClientRect();
+
+    translate.forEach(element => {
+            let speed = element.dataset.speed;
+            element.style.transform = `translateY(${scroll * speed}px)`;
+        })
+
+        big_title.style.opacity = - scroll / (header_height / 2) + 1;
+        shadow.style.height = `${scroll * 0.5 + 300}px`;
+        
+        content.style.transform = `translateY(${scroll  (section_height + sectionY.top) * 50 - 50}px)`;
+})
+
+closeBtn.addEventListener("click", () =>
+{
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
+
+searchBtn.addEventListener("click", () =>
+{
+  menuBtnChange();
+});
